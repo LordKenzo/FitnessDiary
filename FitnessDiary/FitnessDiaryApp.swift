20ae1fba663d57c2eae38ec92d085aa140dd1c54
LordKenzo <lordkenzo@outlook.com>
2025-11-16 09:42:53 +0100

    Initial commit

---
+++ FitnessDiary/FitnessDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessDiaryApp.swift
+//  FitnessDiary
+//
+//  Created by Lorenzo Franceschini on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
